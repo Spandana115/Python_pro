7e4ec7bce7f36a556228ecf71c80ffe8f3e90dec
Spandana115 <baindlaspandana0115@gmail.com>
2025-01-06 16:49:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-variable "vpc_cidr" {
-  default = "10.0.0.0/16"
-}
-
-variable "subnet1_cidr" {
-  default = "10.0.1.0/24"
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
 
 resource "aws_vpc" "demovpc" {
   cidr_block       = var.vpc_cidr
